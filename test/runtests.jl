fffcd627806b31737befc71e11d1c8542465f6ae
ohinder <ohinder@gmail.com>
2015-11-18 14:11:30 -0800

    First commit

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,14 @@ using my_advanced_timer
 using Base.Test
 
 # write your own tests here
-@test 1 == 1
+begin
+  reset_advanced_timer()
+  start_advanced_timer();
+  start_advanced_timer("test");
+  pause_advanced_timer("test");
+  start_advanced_timer("takes time");
+  dot(rand(100000),rand(100000))
+  pause_advanced_timer("takes time");
+  pause_advanced_timer();
+  print_timer_stats()
+end
